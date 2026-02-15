c25b99438c03cbce87973c8566eb4278323bfbd1
neonillamoraru <neonillamoraru@gmail.com>
2026-02-15 15:45:24 +0200

    персонаж прыгать бегать

---
+++ wedma/Assets/Invector-3rdPersonController_LITE/3D Models/Characters/Invector@V-Bot 2.0/FBX/CursorLockToggle.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CursorLockToggle : MonoBehaviour
+{
+    private bool isCursorLocked = true;
+
+    void Start()
+    {
+        LockCursor();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isCursorLocked)
+            {
+                UnlockCursor();
+            }
+            else
+            {
+                LockCursor();
+            }
+        }
+    }
+
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isCursorLocked = true;
+    }
+
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isCursorLocked = false;
+    }
+}
